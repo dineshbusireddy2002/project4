9666ee2fb9b1539b5a964669c0c2e90f170b6021
Dinesh Kumar Reddy Busireddy <111351354+dineshbusireddy2002@users.noreply.github.com>
2024-02-26 19:08:19 +0530

    PGP Project 4

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculateResult() {
+    try {
+      document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+// ... (previous functions) ...
+
+function deleteLastCharacter() {
+    let currentValue = document.getElementById('display').value;
+    document.getElementById('display').value = currentValue.slice(0, -1);
+  }
